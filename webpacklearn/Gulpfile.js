3ac0b11a61b201d262022c82d5909174417234b3
zouchengzhuo <405966530@qq.com>
2016-01-13 22:42:34 +0800

    Signed-off-by: zouchengzhuo <405966530@qq.com>

---
+++ webpacklearn/Gulpfile.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2015/11/29.
+ */
+var gulp = require('gulp');
+var webpack = require("gulp-webpack");
+var webpackConfig = require("./webpack.config.js");
+
+gulp.task('webpack', function () {
+    var myConfig = Object.create(webpackConfig);
+    return gulp
+        .src('./src/main.js')
+        .pipe(webpack(myConfig))
+        .pipe(gulp.dest('./build'));
+});
+
+// 注册缺省任务
+gulp.task('default', ['webpack']);
